58c715dd7f6d336b6c5bc19ba7a84a9b611e16d1
Chinmay Dandekar <chinmay2001.cd@gmail.com>
2022-10-11 22:44:18 +0530

    First Commit

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+          {/* <div>
+            <Image />
+          </div>       */}
+
+    </div>
+  )
+}
+
+export default Banner
